aaebd169e0f0e897ab67a7741d46e5efeb059847
Tanmay <tanmayrela@mac.local>
2022-08-15 16:20:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+
+import './App.css';
+
+import {useEffect,useState} from 'react';
+import jwt_decode from 'jwt-decode';
+
+
+function App() {
+  
+  const [user,setUser]=useState({});
+
+  function handleCallBackResponse(response){
+    console.log("Encoded JWT ID token:" + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden=true;
+  
+  }
+  function handleSignOut(e){
+    setUser({});
+    document.getElementById("signInDiv").hidden=false;
+  }
+
+
+
+  useEffect(()=>{
+    /* global google */
+    google.accounts.id.initialize({
+      client_id:process.env.React_App_Client_Id,
+      callback: handleCallBackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "outline",size:"large"}
+  
+    );
+
+    google.accounts.id.prompt();
+
+  },[]);
+
+
+  return (
+    <div className="App">
+      <div id ="signInDiv"></div>
+      {
+        Object.keys(user).length !=0 &&
+        <button onClick={(e)=> handleSignOut(e)}>Sign Out</button>
+      }
+      
+      
+      {user && 
+      <div>
+        <img src ={user.picture}></img>
+        <h3>{user.name}</h3>
+        </div>
+       }
+    </div>
+  );
+}
+
+export default App;
